70730ff6331fb8560d011b755b91c615ca5dbbfe
14Rahul <rahulgupta141998@gmail.com>
2023-02-05 20:27:57 +0530

    Jenkins Files and script

---
+++ azurescript.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo pkill -f 'java -jar'
+cd /home/azureuser/azagent/_work/r3/a/_Stage2Release/
+rm *
+wget $(pjar) &> /dev/null
+if [[ "$?" != 0 ]]; then
+    exit 1
+else
+    echo "Success"
+fi
+sudo java -jar -Dserver.port=80 \
+*.jar > /dev/null 2> /dev/null < /dev/null &
+
+# create Library Variable group and create the variable and access it in release
